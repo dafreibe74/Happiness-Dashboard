691f5dc7574a9b9dabb617a84e3d287a429a1cc0
NathanBeatheaMartinez <125851733+NathanBeatheaMartinez@users.noreply.github.com>
2023-06-23 00:06:11 -0400

    updating app.py adding country code functionality

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, jsonify, render_template
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:////Users/JA/Desktop/Class_Projects_Data_Science_BC/EDDIES_3/Happiness-Dashboard/data/Happiness.sqlite")
+engine = create_engine("sqlite:///data/Happiness_PF.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -21,6 +21,7 @@ Base.prepare(engine, reflect=True)
 # Save references to each table
 Bands = Base.classes.Bands_2021
 Happiness = Base.classes.Happiness_2021
+Codes = Base.classes.Country_Codes
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -30,9 +31,9 @@ session = Session(engine)
 #################################################
 app = Flask(__name__)
 
-# @app.route("/")
-# def welcome():
-#     return render_template("index.html")
+@app.route("/")
+def welcome():
+    return render_template("index.html")
 
 # @app.route("/api/v1.0/happiness/scores")
 # def scores():
@@ -80,32 +81,34 @@ app = Flask(__name__)
 
 @app.route("/api/v1.0/happiness/data")
 def happinessData():
-    happiness_results = session.query(Happiness.Country, Happiness.Happiness, Happiness.GDP_per_capita, Happiness.Social_support, Happiness.Life_expectancy, Happiness.Freedom, Happiness.Generosity, Happiness.Corruption, Happiness.Dystopia, Happiness.Contribution_GDP_per_capita, Happiness.Contribution_Social_support, Happiness.Contribution_Life_expectancy, Happiness.Contribution_Freedom, Happiness.Contribution_Generosity, Happiness.Contribution_Corruption, Happiness.Contribution_Dystopia, Bands.Metal_bands_per_100000_people, Bands.Bands, Bands.Population).all()
+    joined_results = session.query(Happiness, Bands, Codes).join(Bands, Happiness.Country == Bands.Country).join(Codes, Happiness.Country == Codes.Country).all() #filter(Happiness.Country == Bands.Country).filter(Happiness.Country == Codes.Country).all()
     
     happiness_data_list = []
 
-    for result in happiness_results:
-        country = result[0]
-        happiness_score = result[1]
-        gdp = result[2]
-        social_support = result[3]
-        life_expectancy = result[4]
-        freedom = result[5]
-        generosity = result[6]
-        corruption = result[7]
-        dystopia = result[8]
-        contribution_gdp = result[9]
-        contribution_social_support = result[10]
-        contribution_life_expectancy = result[11]
-        contribution_freedom = result[12]
-        contribution_generosity = result[13]
-        contribution_corruption = result[14]
-        contribution_dystopia = result[15]
-        metal_bands_per_100000_people = result[16]
-        bands = result[17]
-        population = result[18]
+    for result in joined_results:
+        country = result[0].Country
+        happiness_score = result[0].Happiness
+        gdp = result[0].GDP_per_capita
+        social_support = result[0].Social_support
+        life_expectancy = result[0].Life_expectancy
+        freedom = result[0].Freedom
+        generosity = result[0].Generosity
+        corruption = result[0].Corruption
+        dystopia = result[0].Dystopia
+        contribution_gdp = result[0].Contribution_GDP_per_capita
+        contribution_social_support = result[0].Contribution_Social_support
+        contribution_life_expectancy = result[0].Contribution_Life_expectancy
+        contribution_freedom = result[0].Contribution_Freedom
+        contribution_generosity = result[0].Contribution_Generosity
+        contribution_corruption = result[0].Contribution_Corruption
+        contribution_dystopia = result[0].Contribution_Dystopia
+        metal_bands_per_100000_people = result[1].Metal_bands_per_100000_people
+        bands = result[1].Bands
+        population = result[1].Population
+        iso_a2 = result[2].iso_a2
+        iso_a3 = result[2].iso_a3
         
-        happiness_data = [{
+        happiness_data = {
             "country": country,
             "happiness score": happiness_score,
             "gdp": gdp,
@@ -124,8 +127,10 @@ def happinessData():
             "contribution_dystopia": contribution_dystopia,
             "metal_bands_per_100000_people": metal_bands_per_100000_people,
             "bands": bands,
-            "population": population
-            }]
+            "population": population,
+            "iso_a2": iso_a2,
+            "iso_a3": iso_a3
+            }
 
         happiness_data_list.append(happiness_data)
 
