a58c1acd448ec0bbdf5ba997e26025c72b5cf103
ferrjess <125903996+ferrjess@users.noreply.github.com>
2023-06-26 18:27:03 -0500

    added happiness map

--- maps/map1/app.js
+++ maps/map1/app.js
@@ -1,44 +1,58 @@
-d3.json('complete_js.json', function(err, rows){
-      function unpack(rows, key) {
-          return rows.map(function(row) { return row[key]; });
+d3.csv('for_maps.csv')
+  .then(function(rows) {
+    if (!Array.isArray(rows)) {
+      throw new Error('Invalid data format. Expected an array.');
+    }
+
+    function unpack(rows, key) {
+      return rows.map(function(row) {
+        return row[key];
+      });
+    }
+
+    var data = [{
+      type: 'choropleth',
+      locations: unpack(rows, 'CODE'),
+      z: unpack(rows, 'HAPPINESS'),
+      text: unpack(rows, 'COUNTRY'),
+      colorscale: [
+        [0, 'rgb(5, 10, 172)'],
+        [2, 'rgb(40, 60, 190)'],
+        [4, 'rgb(70, 100, 245)'],
+        [6, 'rgb(90, 120, 245)'],
+        [8, 'rgb(106, 137, 247)'],
+        [10, 'rgb(220, 220, 220)']
+      ],
+      autocolorscale: false,
+      reversescale: true,
+      marker: {
+        line: {
+          color: 'rgb(180,180,180)',
+          width: 0.5
+        }
+      },
+      tick0: 0,
+      zmin: 0,
+      dtick: 1000,
+      colorbar: {
+        autotic: false,
+        title: 'Happiness Score'
       }
+    }];
 
-       var data = [{
-            type: 'choropleth',
-            locations: unpack(rows, 'iso-a3'),
-            z: unpack(rows, 'GDP (BILLIONS)'),
-            text: unpack(rows, 'Country'),
-            colorscale: [
-                [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-                [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-                [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
-            autocolorscale: false,
-            reversescale: true,
-            marker: {
-                line: {
-                    color: 'rgb(180,180,180)',
-                    width: 0.5
-                }
-            },
-            tick0: 0,
-            zmin: 0,
-            dtick: 1000,
-            colorbar: {
-                autotic: false,
-                tickprefix: '$',
-                title: 'GDP<br>Billions US$'
-            }
-      }];
+    var layout = {
+      title: 'Global Happiness Scores 2021',
+      geo: {
+        showframe: false,
+        showcoastlines: false,
+        projection: {
+          type: 'mercator'
+        }
+      }
+    };
 
-      var layout = {
-          title: '2014 Global GDP<br>Source: <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html"> CIA World Factbook</a>',
-          geo:{
-              showframe: false,
-              showcoastlines: false,
-              projection:{
-                  type: 'mercator'
-              }
-          }
-      };
-      Plotly.newPlot("myDiv", data, layout, {showLink: false});
-});
+    Plotly.newPlot('myDiv', data, layout, { showLink: false });
+  })
+  .catch(function(error) {
+    console.error('Error:', error);
+  });
